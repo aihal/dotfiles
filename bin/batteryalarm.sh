b2cadc50e4d22f8a54841c2fb0c1182406ee5002
Ogion <aihal@lavabit.com>
2011-11-12 11:10:02 +0100

    changing from .Xdefaults to .Xresources (same content, just different place

--- bin/batteryalarm.sh
+++ bin/batteryalarm.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # script sends alarm when battery low
-export DISPLAY=":0"
+#export DISPLAY=":0"
 batterylevel=$(acpi -b | cut -d ' ' -f 4 | tr -d '%,')
 
 battery_alarm() {
-#    zenity --warning --text="Achtung Akku ist nur noch bei ${batterylevel}%"
-    echo "naughty.notify{text=\"Achtung: Akku ist nur noch bei ${batterylevel}%\", fg=\"#F92672\", timeout=0}" | awesome-client
+    #zenity --warning --text="Achtung Akku ist nur noch bei ${batterylevel}%"
+    notify-send -t 0 "Batteryalarm" "Achtung: Akku nur noch bei ${batterylevel}%" -u CRITICAL
+    #echo "naughty.notify{text=\"Achtung: Akku ist nur noch bei ${batterylevel}%\", fg=\"#F92672\", timeout=0}" | awesome-client
 }
 
-[[ $batterylevel -le 15 ]] && battery_alarm
+#[[ $batterylevel -le 15 ]] && battery_alarm
+
+if [[ $batterylevel -le 15 ]]
+then
+    battery_alarm
+fi
