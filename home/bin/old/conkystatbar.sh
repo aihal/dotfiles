e58d32a8e68578068f9353f0881ed3c5d0a13f01
Ogion <ogion@openmailbox.org>
2014-11-02 11:23:05 +0100

    cleaning up old files, adding new ones etc

--- home/bin/old/conkystatbar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/dash
-conky | dzen2 -expand r -ta r -fn -*-profont-*-*-*-*-14-*-*-*-*-*-*-* -y 585 -x 250 # -w 800
--- home/bin/old/i3wsbar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/dash
-i3-wsbar -c "dzen2 -fn -*-profont-*-*-*-*-14-*-*-*-*-*-*-* -expand r -ta -l -y 555"
