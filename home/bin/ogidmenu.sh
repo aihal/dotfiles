7f7db20f9e0a99fb528310c2effaaa3a224c49fd
Ogion <ogion@openmailbox.org>
2014-12-14 15:45:56 +0100

    various

--- home/bin/ogidmenu.sh
+++ home/bin/ogidmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#exec dmenu_run -l 10 -nb "#92AFDE" -nf "#000000" -sb "#D61C41" -sf "#FFFFFF" 
-rofi -rnow
+exec dmenu_run -l 10 #-nb "#92AFDE" -nf "#000000" -sb "#D61C41" -sf "#FFFFFF" 
+#rofi -rnow
