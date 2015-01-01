cdb9191d501972edb57deb6980ed83d052e5555c
Ogion <ogion@openmailbox.org>
2015-01-01 10:29:06 +0100

    small changes to scrot_ogi.sh and i3blocks.conf

--- home/bin/scrot_ogi.sh
+++ home/bin/scrot_ogi.sh
@@ -1,2 +1,4 @@
-#!/bin/dash
-sleep 0.03 && scrot '%Y-%m-%d-%H_%M_%S.png' -e 'mv $f ~/bilder/Screenshots/2014/' && notify-send -t 1000 "screenshot taken" 
+#!/bin/sh
+thisyear=$(date +"%Y")
+[ ! -d ~/bilder/Screenshots/$thisyear ] && mkdir -p ~/bilder/Screenshots/$thisyear
+sleep 0.03 && scrot '%Y-%m-%d-%H_%M_%S.png' -e "mv \$f ~/bilder/Screenshots/$thisyear/" && notify-send -t 1000 "screenshot taken" 
