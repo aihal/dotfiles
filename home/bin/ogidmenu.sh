020d30a1e21a9ba048cb1d5acff90761a1557d2e
Ogion <ogion@openmailbox.org>
2015-02-20 21:19:14 +0100

    color theme changes in .i3/config and ogidmenu.sh, added i3blocks script .i3/bin/volume

--- home/bin/ogidmenu.sh
+++ home/bin/ogidmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec dmenu_run -l 10 #-nb "#92AFDE" -nf "#000000" -sb "#D61C41" -sf "#FFFFFF" 
+exec dmenu_run -l 10  -sb "#E57910" #-nb "#92AFDE" -nf "#000000" -sf "#FFFFFF" 
 #rofi -rnow
