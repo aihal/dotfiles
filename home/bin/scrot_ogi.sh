52743d4ec77f8d8a3708209f7c8155cb77a0ff25
Ogion <ogion@openmailbox.org>
2019-03-15 10:17:15 +0100

    randombg desktop wallpaper changer, various changes

--- home/bin/scrot_ogi.sh
+++ home/bin/scrot_ogi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 thisyear=$(date +"%Y")
 [ ! -d ~/bilder/Screenshots/$thisyear ] && mkdir -p ~/bilder/Screenshots/$thisyear
-sleep 0.03 && scrot '%Y-%m-%d-%H_%M_%S.png' -e "mv \$f ~/bilder/Screenshots/$thisyear/" && notify-send -t 1000 "screenshot taken" 
+sleep 0.3 && scrot '%Y-%m-%d-%H_%M_%S.png' -e "mv \$f ~/bilder/Screenshots/$thisyear/" && notify-send -t 1000 "screenshot taken" 
